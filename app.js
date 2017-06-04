20f266b7d91935ab27d4def3d2db8e58a09407d7
Robert <robert.p1991@gmail.com>
2017-06-04 20:51:00 +0200

    setup db connection

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
-
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mydb', 'robert', 'robert', {
+  host: 'mysql',
+  port: 3306,
+  dialect: 'mysql'
+});
 const PORT = 3030;
 
-app.get('/', function (req, res) {
+sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch((err) => {
+  console.error('Unable to connect to the database:', err);
+});
+
+
+app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('Running on http://localhost:3000');
